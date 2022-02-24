cad9e3ec685cef27aa7e44331f8807200f15f429
Robert Bartlett <amagon270@gmail.com>
2022-02-24 11:44:00 +1000

    added cors

--- pages/api/export.js
+++ pages/api/export.js
@@ -1,7 +1,29 @@
 import { PrismaClient } from '@prisma/client'
 import { createClient } from '@supabase/supabase-js'
 
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors)
   const prisma = new PrismaClient();
   if (req.method === "GET") {
     try {
